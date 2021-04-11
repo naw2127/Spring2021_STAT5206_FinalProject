521a4b7b2e2d69b16ff3ff40b705f101d3be144c
naw2127 <58568611+naw2127@users.noreply.github.com>
2021-04-11 12:09:16 -0400

    some changes to analyse crops and clean crops

--- lib/analyze_crops.R
+++ lib/analyze_crops.R
@@ -3,7 +3,7 @@
 ## Script name: analyze_crops
 ##
 ## Purpose of script: Analyze crop yield data cleaned by clean_crops.R and 
-## saved to ./output/XXX.csv
+## saved to ./output/XXX.csv, create figs and plots
 ##
 ## Author: Natalie Williams
 ##
@@ -11,7 +11,9 @@
 ##
 ## ---------------------------
 ##
-## Notes: K means defies hypothesis
+## Notes: This script has been modified to only generate data frames 
+## Functions for plotting are in graph_crops.R
+## All functions from earlier versions were moved to bottom 
 ##
 ## ---------------------------
 
@@ -23,32 +25,59 @@ setwd(natalie_wd)     # Natalie's working directory (mac)
 
 ## load up the packages we will need: 
 library(tidyverse)
-library(e1071)       # for graphing and some simple model building
 library(usmap)       # for plotting kmeans resutls on map
+library(broom)       # for getting slopes of trendlines
 
 ## ---------------------------
 ## Read in cleaned data from ./output/midwest_crop_yields.csv
 
-midwest_cy <- read_csv("./output/midwest_crop_yields.csv")
-head(midwest_cy, 5)
+midwest_crops <- read_csv("./output/midwest_crop_yields.csv")
+head(midwest_crops, 5)
 
 
 ## ---------------------------
-## Question 1: How are crop yields changing nationally? 
-g1 <- ggplot(midwest_cy, aes(x=year, y=nat_avg))+
-  geom_point()+
-  geom_smooth()+
+# Details https://www.rdocumentation.org/packages/broom/versions/0.7.5/topics/glance.lm
+
+pct_diff_m <- midwest_cy %>%
+  na.omit()%>%
+  group_by(state) %>%
+  do(glance(lm(avg ~ year, data = .)))
+
+preview <- select(pct_diff_m, c(state, r.squared))%>%
+  arrange(r.squared)
+mean.r <- mean(preview$r.squared, na.rm= TRUE)
+head(preview)
+ggplot(preview, aes(x=r.squared))+
+  geom_density()+
+  geom_vline(xintercept = mean.r)
+
+# R square's values are super low
+
+## ---------------------------
+## New approach - compare diff between pct_chg and nat_pct_chg
+
+growth_diff <- midwest_cy%>%
+  na.omit()%>%
+  mutate(
+    growth_diff = pct_chg- nat_pct_chg
+  )%>%
+  select(year,state,region, growth_diff)
+
+## ---------------------------
+## Graph of growth diff over time
+g7 <- ggplot(growth_diff, aes(x=year, y=growth_diff, color=state))+
+  geom_point(size=0.5)+
+  geom_smooth(se=FALSE)+
+  scale_y_continuous(labels = function(x) paste0(x, "%"), limits = c(-25,25))+
+  facet_wrap(~region, ncol =2)+
   labs(
-    title = "How have National Corn Yields varied since 1975?",
-    x= "Year",
-    y= "Corn Bushels per Acre"
-  )+
-  theme_classic()
+    title = "State's Growth Compared to National Average Growth", 
+    subtitle = "Northern States have a smaller distribution, but this could be due to smaller group size",
+    y= "Percent Change")
 
-g1
-ggsave(path = "./figs", filename = "national_cy.png")
+g7
 
-## Question 1a: How can the relationship of year and national avg be correlated? 
+## Question 1b: How can the relationship of year and national avg be correlated? 
 linearMod <- lm(nat_avg ~ year, data=midwest_cy)  # build linear regression model on full data
 print(linearMod)
 
@@ -101,8 +130,7 @@ g2 <- ggplot() +
   facet_wrap(~cluster, nrow = 1) + 
   labs(
     title = "Average Crop Yield of US States",
-    y= "Corn Bushels per Acre") +
-  scale_x_continuous(breaks = seq(from = 1975, to = 2020, by = 05))
+    y= "Corn Bushels per Acre")
 
 g2
 ggsave(path = "./figs", filename = "cluster_graph.png")
@@ -123,4 +151,122 @@ dev.off()  # when this line is run, the png is saved
 
 ## ---------------------------
 ## Next steps
+
+## ---------------------------
+## Question 1: How are crop yields changing nationally? 
+g1 <- ggplot(midwest_cy, aes(x=year, y=nat_avg))+
+  geom_point()+
+  geom_smooth()+
+  labs(
+    title = "How have National Corn Yields varied since 1975?",
+    x= "Year",
+    y= "Corn Bushels per Acre"
+  )+
+  theme_classic()
+
+g1
+ggsave(path = "./figs", filename = "national_cy.png")
+
+## ---------------------------
+## Question 1a: What is the distribution of the states % change?
+g2 <- ggplot(na.omit(select(midwest_cy, c(year,state, pct_chg))), aes(x=pct_chg))+
+  geom_density()+
+  facet_wrap(~state)+
+  scale_y_continuous(labels = function(x) paste0(x, "%"))+
+  labs(
+    title = "Distribution of % Change from 1975-2020", 
+    subtitle = "As expected, most states have a mode of 0% and are slightly right skewed")
+
+g2
+ggsave(path = "./figs", filename = "dist_of_pct_chg.png")
+
+## ---------------------------
+## Question 1b: Percent Change over time
+g3_dat <- na.omit(select(midwest_cy, c(year,state,region, pct_chg)))
+g3 <- ggplot(g3_dat, aes(x=year, y=pct_chg, color=state))+
+  geom_point(size=0.5)+
+  geom_smooth()+
+  scale_y_continuous(labels = function(x) paste0(x, "%"))+
+  facet_wrap(~region)+
+  labs(
+    title = "States % Change of Corn Yields from 1975 to 2020", 
+    subtitle = "Northern States have a smaller distribution, but this could be due to smaller group size",
+    y= "Percent Change")
+
+g3
+ggsave(path = "./figs", filename = "pct_chg_over_time.png")
+
+## Question 1c: Percent Change over time another way to visualize
+g4 <- ggplot(g3_dat, aes(x=year, y=pct_chg, color=state))+
+  geom_line()+
+  scale_y_continuous(labels = function(x) paste0(x, "%"))+
+  facet_wrap(~region, nrow=2)+
+  labs(
+    title = "States % Change of Corn Yields from 1975 to 2020", 
+    subtitle = "Northern States have a smaller distribution, but this could be due to smaller group size",
+    y= "Percent Change")
+
+g4
+ggsave(path = "./figs", filename = "pct_chg_over_time_line.png")
+
+## Question 1d: Percent Difference from National Average over time 
+g5_dat <- na.omit(select(midwest_cy, c(year,state,region, pct_diff))) 
+
+g5 <- ggplot(g5_dat, aes(x=year, y=pct_diff, color=state))+
+  geom_point(size=0.5)+
+  geom_smooth(se=FALSE)+
+  geom_hline(yintercept = 0, linetype= "dashed", color="black", size=1.5)+
+  scale_y_continuous(labels = function(x) paste0(x, "%"))+
+  facet_wrap(~region, ncol=2)+
+  labs(
+    title = "% Difference from National Average Corn Yield", 
+    subtitle = "Differences in trends between regions are hard to glean")
+
+g5
+ggsave(path = "./figs", filename = "pct_diff.png")
+
+## Question 1e: Percent Difference from National Average over time - trend lines
+
+g6 <- ggplot(g5_dat, aes(x=year, y=pct_diff, color=state))+
+  geom_point(size=0.5)+
+  geom_smooth(method="lm", se=FALSE)+
+  geom_hline(yintercept = 0, linetype= "dashed", color="black", size=1.5)+
+  scale_y_continuous(labels = function(x) paste0(x, "%"))+
+  facet_wrap(~region, ncol=2)+
+  labs(
+    title = "% Difference from National Corn Yield", 
+    subtitle = "Northern States have a smaller distribution, but this could be due to smaller group size",
+    y= "Percent Difference from the National Average Corn Yield")
+
+g6
+ggsave(path = "./figs", filename = "pct_diff_trend_lines.png")
+
+## Divide g6 into two plots for better legend reading
+g6a <- ggplot(filter(g5_dat, region=="north"), aes(x=year, y=pct_diff, color=state))+
+  geom_point(size=0.5)+
+  geom_smooth(method="lm", se=FALSE)+
+  geom_hline(yintercept = 0, linetype= "dashed", color="black", size=1.5)+
+  scale_y_continuous(labels = function(x) paste0(x, "%"))+
+  labs(
+    title = "% Difference from National Corn Yield", 
+    subtitle = "Northern states are collectively trending upwards, except 
+                       Wisconsin, however Wisconsin remains above the national average",
+    y= "Percent Difference from the National Average Corn Yield"
+  )
+
+g6a
+g6b <- ggplot(filter(g5_dat, region=="south"), aes(x=year, y=pct_diff, color=state))+
+  geom_point(size=0.5)+
+  geom_smooth(method="lm", se=FALSE)+
+  geom_hline(yintercept = 0, linetype= "dashed", color="black", size=1.5)+
+  scale_y_continuous(labels = function(x) paste0(x, "%"))+
+  labs(
+    title = "% Difference from National Corn Yield", 
+    subtitle = "Southern States except for Arkansas, Tennessee, and Missouri are trending downwards",
+    y= "Percent Difference from the National Average Corn Yield"
+  )
+
+g6b
+ggsave(path = "./figs", filename = "south_pct_diff.png")
+
  
--- lib/clean_crops.R
+++ lib/clean_crops.R
@@ -24,59 +24,104 @@ setwd(natalie_wd)     # Natalie's working directory (mac)
 
 ## load up the packages we will need: 
 library(tidyverse)
+library(priceR) # for price adjustment
+
 
 ## ---------------------------
 ## Function to select and rename desired columns
-cln_dat <- function(raw_data){
-  cleaned_data <- raw_data %>%
+cln_dat <- function(raw_dat){
+  cleaned_data <- raw_dat %>%
     select(Year, State, County, "Data Item", Value, "CV (%)")%>%
     rename("year" = "Year", "state"="State", "county"="County", 
-           "data_item"= "Data Item", "value"="Value", "cv" = "CV (%)")
+           "data_item"= "Data Item", "value"="Value", "cv" = "CV (%)")%>%
+    filter(state != "IDAHO")
   return(cleaned_data)
 }
 
 ## Function to get state statistics
 ## Make state regions
 
-states <- unique(midwest$State)
+# states <- unique(midwest$State)    for purposes of making north and south
 north <- c("IDAHO","MINNESOTA","MONTANA", "NORTH DAKOTA",
            "SOUTH DAKOTA", "WISCONSIN")
-south <- c("ARKANSAS","ILLINOIS", "INDIANA", "IOWA", "KANSAS",
-           "KENTUCKY", "MISSOURI", "NEBRASKA", "OKLAHOMA",
+
+mid <- c("ILLINOIS", "INDIANA", "IOWA", "NEBRASKA", "KANSAS",
+         "KENTUCKY", "MISSOURI")
+
+south <- c("ARKANSAS", "OKLAHOMA",
            "TENNESSEE", "TEXAS")
+
 
 state_stats <- function(cln_dat){
   # get state stats 
   state_stats <- cln_dat %>%
+    arrange(year,state)%>%
     group_by(year, state)%>%
     summarize(
       avg = mean(value),
+      med = median(value),
+      total = sum(value),
       max = max(value),
       min = min(value)
     )%>%
     mutate(
       region = case_when(
         state %in% north ~ "north",
+        state %in% mid ~ "mid",
         state %in% south ~ "south"
       )
     )
-  # get national average of each year
+  # Need to reorganize data to get the pct changes of each state
+  pct_chgs <- distinct(state_stats, year, state, avg)%>%
+    group_by(state)%>%
+    mutate(
+      pct_chg = (avg/lag(avg)-1)*100
+    )
+  state_stats <- merge(state_stats, pct_chgs)
+  
+  # get national average and % change of each year
   nat_stats <- state_stats%>%
     group_by(year)%>%
     summarize(
-      nat_avg = mean(avg) 
+      nat_avg = mean(avg)
+    )%>%
+    mutate(
+      nat_pct_chg = (nat_avg/lag(nat_avg) - 1) * 100
     )
-  
+
   # once national trends are known, get % diff from national avg 
   state_stats <- merge(state_stats, nat_stats) %>%
     mutate(
-      pct_diff = (avg - nat_avg)/ nat_avg
-    )
+      pct_diff = ((avg - nat_avg)/ nat_avg) * 100,
+    )%>%
+    select(year,state, region, avg, med, total, max,
+           min, pct_chg, nat_avg,
+           pct_diff, nat_pct_chg)
+
+  #return(state_stats)
+  return(state_stats)
+}
+
+
+sale_stats <- function(cln_dat){
+  state_stats <- cln_dat %>%
+    mutate(
+      region = case_when(
+        state %in% north ~ "north",
+        state %in% mid ~ "mid",
+        state %in% south ~ "south"
+      )
+    )%>%
+    mutate(
+      adj_value = adjust_for_inflation(value, from_date = year, "US", to_date = 2019)
+    )%>%
+    select(-c(county, cv))
+  ## Adjust for inflation using CPI BLS 
+  ## Source: https://stackoverflow.com/questions/12590180/inflation-adjusted-prices-package
   
   return(state_stats)
 }
 ## ---------------------------
-## IF READING IN DATASET OF ALL STATES  ((PREFERRED))
 ## Clean Dataset of all midwest states from 1975-2020
 midwest1 <- read_csv("./data/NASS_Ar_Kn_1975_2020_CY.csv")  # had to break it up
 midwest2 <- read_csv("./data/NASS_Mn_ws_1975_2020_CY.csv")  # to download
@@ -84,6 +129,75 @@ midwest <- rbind(midwest1, midwest2)                        # recombine
 cln_midwest <- cln_dat(midwest)
 
 midwest_stats <- state_stats(cln_midwest)
+head(filter(midwest_stats, state=="KANSAS"))
+tail(midwest_stats, 5)
 
-head(midwest_stats, 5)
+
 write.csv(midwest_stats, file= "./output/midwest_crop_yields.csv")
+
+## Read in Crop sales data 
+raw_sales <- read_csv("./data/NASS_1975_2020_sales.csv")
+cln_sales <- cln_dat(raw_sales)
+
+midwest_sales <- sale_stats(cln_sales)
+write.csv(midwest_sales, file= "./output/midwest_sales.csv")
+
+## ---------------------------
+## Methods for standardizing clean data -- ENDED UP NOT USING -- 
+
+## Standarize using stand_avg = (avg-min) / (max-min)
+standardize <- function(cln_dat){
+  # get standarized state stats 
+  stand_stats <- cln_dat %>%
+    arrange(year,state)%>%
+    group_by(year, state)%>%
+    summarize(
+      avg = mean(value),
+      max = max(value),
+      min = min(value)
+    )%>%
+    mutate(
+      stand_avg = (avg-min) / (max-min),
+      region = case_when(
+        state %in% north ~ "north",
+        state %in% south ~ "south"
+      )
+    )
+  # Need to reorganize data to get the pct changes of each state
+  pct_chgs <- distinct(stand_stats, year, state, stand_avg)%>%
+    group_by(state)%>%
+    mutate(
+      stand_pct_chg = (stand_avg/lag(stand_avg)-1)*100
+    )
+  stand_stats <- merge(stand_stats, pct_chgs)
+  # get standardized national average and % change of each year
+  nat_stats <- stand_stats%>%
+    group_by(year)%>%
+    summarize(
+      min_state = min(avg),
+      max_state = max(avg),
+      nat_avg = mean(avg)
+    )%>%
+    mutate(
+      stand_nat_avg = (nat_avg-min_state) / (max_state-min_state),
+      stand_nat_pct_chg = (stand_nat_avg/ lag(stand_nat_avg) -1) *100,
+    )
+  
+  # once national trends are known, get % diff from national avg 
+  stand_stats <- merge(stand_stats, nat_stats) %>%
+    mutate(
+      stand_pct_diff = ((stand_avg - stand_nat_avg)/ stand_nat_avg) * 100,
+    )%>%
+    select(year,state, region, stand_avg, stand_pct_chg, stand_nat_avg,
+          stand_pct_diff, stand_nat_pct_chg)
+  
+  
+  return(stand_stats)
+} 
+
+
+stan_stats <- standardize(cln_midwest)
+head(filter(stan_stats, state=="KANSAS"))
+write.csv(stan_stats, file= "./output/standardized_crop_yields.csv")
+
+
