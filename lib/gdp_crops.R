0a9ff16379da92083fbfe8a5f03e4be38872cf52
naw2127 <58568611+naw2127@users.noreply.github.com>
2021-04-11 15:13:15 -0400

    script for graphing crop and econ data together

--- lib/gdp_crops.R
+++ lib/gdp_crops.R
@@ -32,4 +32,35 @@ SIC_stats <- read_csv("./output/SIC_stats.csv")
 NAICS_stats <- read_csv("./output/NAICS_stats.csv")
   
 ## ---------------------
-## 
+## Check data make sure all regions are there 
+
+head(crops)
+head(sales)
+head(gdp)
+head(SIC_stats)
+head(NAICS_stats)
+
+# Check regions   SIC_stats, gdp, SIC_stats dont have "mid" 
+print(unique(gdp$region))
+check_regions <- filter(gdp, region =='south')
+
+print(unique(check_regions$state))  # Should only be ARKANSAS, OKLAHOMA, TENNESSEE, TEXAS
+
+## ---------------------
+## Some prelim graphs 
+
+## Stats with g % GDP from agriculture 
+NAICS_stats_2019 <- NAICS_stats %>% filter(year == 2019)
+
+head(NAICS_stats)
+
+png("./figs/regions_map.png")  # for saving
+p2 <- plot_usmap(data = NAICS_stats_2019, values = "perc", color = "white")+ 
+  scale_fill_gradient(low =  "lightgreen", high =  "yellow", na.value="lightgray") +
+  labs(
+    title = "% of GDP from Agriculture, Forestry, and Fishing"
+  )+
+  theme(legend.position = "right")
+
+p2
+dev.off() 
